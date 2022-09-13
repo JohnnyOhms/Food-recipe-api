9630bde09c7ec05acfb25a8815a891c556c0d76e
JohnnyOhms <officialjohn662@gmail.com>
2022-09-13 23:35:25 +0100

    api fetched

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
+const form = document.querySelector("#form")
+const input = document.querySelector("#search-box")
+const search_bnt = document.querySelector(".search-btn")
+const inst = document.querySelector(".instruction")
+const searchName = document.querySelector("#search-name")
+const mealContainer = document.querySelector(".meals")
+const ApiURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i="
+const detailsURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+
+
 class GetReciepe{
     async getData() {
-        let res = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=egg")
-        let data = await res.json()
-        return data
+        try {
+            let res = await fetch(`${ApiURL}${input.value.trim()}`)
+            let data = await res.json()
+            return data
+        } catch (error) {
+            alert(`${error} Refreash page`)
+        }
+    }
+}
+
+class Logic{
+    searchReceipe(){
+        search_bnt.addEventListener("click", (e) => {
+            e.preventDefault()
+            getReciepe.getData()
+                .then(data =>logic.displayData(data.meals))
+        } )
     }
 
-    async mealDetails() {
-        let res = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52952")
-        let data = await res.json()
-        return data
+    displayData(data) {
+        let display = ''
+        data.forEach(item => {
+            display += `
+                 <div class="single-meal">
+                    <div class="meal-img">
+                        <img src="${item.strMealThumb}" alt="" srcset="">
+                    </div>
+                    <span>${item.strMeal}</span>
+                    <div class="view-receipe">
+                        <button id="view-receipe" data-id="${item.idMeal}">view-receipe</button>
+                    </div>
+                </div>
+            `
+            mealContainer.innerHTML = display;
+        });
+       
     }
 }
 
 
 const getReciepe = new GetReciepe()
-getReciepe.getData()
-    .then((data) => console.log(data))
-
-getReciepe.mealDetails()
-.then(data=> console.log(data, data.meals[0].strInstructions))
+const logic = new Logic()
+logic.searchReceipe();
